24250e279ef443161422f474d4614754e363981c
osmond <j@osmond.cc>
2026-05-22 19:46:13 -0500

    feat: add image support to writing posts (hero + list thumbnail + OG)

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,6 +9,8 @@ const writing = defineCollection({
     date:        z.coerce.date(),
     tags:        z.array(z.string()).optional().default([]),
     draft:       z.boolean().optional().default(false),
+    image:       z.string().optional(),
+    imageAlt:    z.string().optional(),
   }),
 });
 
